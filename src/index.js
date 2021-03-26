8dd79b65835c1c9c91d58666e7bb1e4ccf981230
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-03-26 17:42:37 +0900

    delete comment

--- src/index.js
+++ src/index.js
@@ -2,23 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: null,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <button className="square" onClick={() =>  this.setState({value: 'X'}) }>
-//         {this.state.value}
-//       </button>
-//     );
-//   }
-// }
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
